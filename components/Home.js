29cf2a02e916f79943014806e6cb93be5a58683e
Houss82 <houssem1982mansour@gmail.com>
2023-11-15 16:42:36 +0100

    first commit

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesData, setMoviesData]= useState([])
 
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://mymoviz-backend-gamma-eight.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         const formatedData = data.map(movie => {
